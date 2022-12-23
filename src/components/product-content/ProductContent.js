079c36b99cf3af065d8970631ed4269387fe6fea
augustinekirumba <steentoonz@gmail.com>
2022-12-23 21:57:47 +0300

    Fixed the auto height issue on the out of stock items

--- src/components/product-content/ProductContent.js
+++ src/components/product-content/ProductContent.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { PureComponent } from 'react';
+import React from "react";
+import { PureComponent } from "react";
 import "../../assets/css/productView.css";
 
 export class ProductContent extends PureComponent {
@@ -14,8 +14,8 @@ export class ProductContent extends PureComponent {
       instockStyle,
       instockForButton,
       checkCartDuplicates,
-      singleAttribute
-    } = this.props
+      singleAttribute,
+    } = this.props;
 
     return (
       <div className="product-view-container">
@@ -23,56 +23,57 @@ export class ProductContent extends PureComponent {
           {printImageThumbnails}
         </div>
         <div className="product-view-image">
-          <div className="product-view-image-div">
-            <img
-              src={ currentProduct.gallery[ thumbnailId ] }
-              alt={ currentProduct.name }
-            />
-          </div>
-          <div 
+          <div
             className="instock-container"
             style={{
-              display: instockStyle(currentProduct.inStock)
+              display: instockStyle(currentProduct.inStock),
             }}
           >
             <div className="instock-wrapper">
               <div className="instock-div">OUT OF STOCK</div>
             </div>
           </div>
+          <div className="product-view-image-div">
+            <img
+              src={currentProduct.gallery[thumbnailId]}
+              alt={currentProduct.name}
+            />
+          </div>
         </div>
         <div className="product-view-details-wrapper">
           <div className="product-view-details-brand">
-            { currentProduct.brand }
+            {currentProduct.brand}
           </div>
-          <div className="product-view-details-name">
-            { currentProduct.name }
-          </div>
+          <div className="product-view-details-name">{currentProduct.name}</div>
           {attributes}
           <div className="product-view-details-price-wrapper">
             <div className="price-name">PRICE: </div>
-            <div className="price">{`${ currencySymbol[1] }${
-              currentProduct.prices[ currencySymbol[0] ]
-                .amount
+            <div className="price">{`${currencySymbol[1]}${
+              currentProduct.prices[currencySymbol[0]].amount
             }`}</div>
           </div>
           <div className="product-view-details-button">
             <button
               onClick={() => {
-                if ( currentProduct.inStock )
-                  checkCartDuplicates(currentProduct, currentProduct.id, singleAttribute);
+                if (currentProduct.inStock)
+                  checkCartDuplicates(
+                    currentProduct,
+                    currentProduct.id,
+                    singleAttribute
+                  );
               }}
               style={{
-                display: instockForButton(currentProduct.inStock)
+                display: instockForButton(currentProduct.inStock),
               }}
             >
               ADD TO CART
             </button>
           </div>
-          <div className="product-view-details-desc">{ parsedDescription }</div>
+          <div className="product-view-details-desc">{parsedDescription}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default ProductContent
+export default ProductContent;
